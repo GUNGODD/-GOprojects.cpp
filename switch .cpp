05f3c3f61a87a907cfde1b6e0c2769b49ea8984a
Aditya Singh <104669514+GUNGODD@users.noreply.github.com>
2022-07-16 14:18:04 +0530

    Add files via upload

---
+++ switch .cpp
@@ -0,0 +1,26 @@
+#include  <iostream> 
+using namespace std;
+int main (){
+    int a = 3;
+    switch (a){
+        case 1:
+        cout << "1 is stored in a"<< endl;
+
+        break;
+        case2:
+          cout << "2 is stored is a" <<  endl;
+        break;
+        case 3:
+        cout << "3 is stored in a " << endl;
+           break;
+        default:
+        cout << "None of the value matched" << endl;
+        
+        
+        
+                break;
+
+    }
+}
+
+
