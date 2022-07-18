13b160c41efde22d8ef8d83929ee0d6cfbc3e54d
Aditya Singh <104669514+GUNGODD@users.noreply.github.com>
2022-07-18 12:24:12 +0530

    Add files via upload

---
+++ WhileLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n;
+    cout << "Enter the number"<< endl;
+    cin >>n;
+     int i =1; // intiailization of the i 
+     while (i <= n) {
+      cout << i << endl;
+      i= i+1;
+   
+
+     }
+
+   return 0;
+
+}
