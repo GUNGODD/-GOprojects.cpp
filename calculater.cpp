d23894a95f73e0dd92bd71c7bc8d72e21e4ec154
Aditya Singh <104669514+GUNGODD@users.noreply.github.com>
2022-07-18 12:30:01 +0530

    Update calculater.cpp

--- calculater.cpp
+++ calculater.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 using namespace std;
 int main () {
- int var1 = 5;
- int var2 = 6;
+ int var1;
+ int var2;
+ cout <<"Enter the A" << endl;
+ cin >> var1;
+ cout << " Enter the B" << endl;
+ cin >> var2;
  cout << (var1 > var2);
-
-
-
-
-
-
-
- 
-
-
-
-
-
+ cout << var1 + var2 << "is the addition " << endl;
+ cout << var1 - var2 << " is the substraciton" << endl;
+ cout << var1 * var2 << "is the multiplication  " << endl;
+ cout << var1 / var2 << "is the division " << endl;
+ cout << var1 % var2 << "is the module" << endl;
 
   
 }
