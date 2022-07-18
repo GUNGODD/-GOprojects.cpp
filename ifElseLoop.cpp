b1b5720eaf98591df13d3900ef40271d7670b955
Aditya Singh <104669514+GUNGODD@users.noreply.github.com>
2022-07-18 14:04:47 +0530

    Add files via upload

---
+++ ifElseLoop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main () {
+  int a , b;
+  cout << "Enter the value of a" << endl;
+  cin >> a;
+  cout << "Enter the value of b" << endl;
+  cin >> b;
+
+
+  if (a>b){
+    cout << a << "is greater then" << b<< endl;
+
+  } else {
+    cout << b << " is greater number then " << a<<endl;
+
+  }
+     
+     
+       return 0;
+
+  
+}
